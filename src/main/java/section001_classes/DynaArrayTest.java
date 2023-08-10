e0a1115cbcb8c6a7723b3e6e830c96d9ced16842
Alexandra Lebedzeva <evtuhova15vek44@gmail.com>
2023-08-10 20:07:21 +0300

    Initial commit

---
+++ src/main/java/section001_classes/DynaArrayTest.java
@@ -0,0 +1,18 @@
+package section001_classes;
+
+import java.util.Arrays;
+
+public class DynaArrayTest {
+    public static void main(String[] args) {
+        DynaArray dynaArray1 = new DynaArray();
+        DynaArray dynaArray2 = new DynaArray();
+
+        System.out.println("dynaArray1: ");
+        System.out.println(Arrays.toString(dynaArray1.result) + " ");
+        System.out.println(dynaArray1.count);
+
+        System.out.println("dynaArray2: ");
+        System.out.println(Arrays.toString(dynaArray2.result) + " ");
+        System.out.println(dynaArray2.count);
+    }
+}
